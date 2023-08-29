2895b6e133d25be005a8445d8f2b67aaa9e48808
quoc loc <loc842211@gmail.com>
2023-08-29 10:50:45 +0700

    create mySql2

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+
+require("dotenv").config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// config view
+configViewEngine(app)
+// init web 
+initWebRoutes(app)
+
+let port = process.env.PORT || 6969;
+app.listen(port,()=>{
+    console.log("check "+port)
+})
