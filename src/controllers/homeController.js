d4f3fb8feb385c74bcd7403a8df455a754767de9
quoc loc <loc842211@gmail.com>
2023-09-05 18:33:40 +0700

    delete

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -27,9 +27,7 @@ let postCRUD = async (req,res)=>{
 }
 let displayGetCRUD = async (req,res)=>{
     let data =await CRUDService.getAllUser();
-    console.log('---------')
-    console.log(data)
-    console.log('--------')
+    
     return res.render('displayCRUD.ejs',{
         dataTable:data
     })
@@ -55,8 +53,16 @@ let putCRUD = async (req,res)=>{
     return res.render('displayCRUD.ejs',{
         dataTable:allUsers
     })
+}
+let deleteCRUD =async (req,res)=>{
+    let id = req.query.id;
+    if(id){
+    await CRUDService.deleteUserById(id)
+    return res.send('delete the usre')
+    }else{
+        return res.send('user not found')
+    }
     
-
 }
 module.exports = {
     getHomePage:getHomePage,
@@ -65,5 +71,6 @@ module.exports = {
     postCRUD:postCRUD,
     displayGetCRUD:displayGetCRUD,
     getEditCRUD:getEditCRUD,
-    putCRUD:putCRUD
+    putCRUD:putCRUD,
+    deleteCRUD:deleteCRUD
 };
