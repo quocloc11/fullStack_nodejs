7e228e8a93bf57383050aa39b0a34819af3c7740
quoc loc <loc842211@gmail.com>
2023-09-05 11:20:15 +0700

    orm phan2

--- src/route/web.js
+++ src/route/web.js
@@ -14,6 +14,8 @@ let initWebRoutes = (app)=>{
     router.get('/about', homeController.getAboutPage)
     router.get('/crud',homeController.getCRUD)
     router.post('/post-crud',homeController.postCRUD)
+    router.get('/get-crud',homeController.displayGetCRUD)
+
     return app.use("/",router)
 }
 
