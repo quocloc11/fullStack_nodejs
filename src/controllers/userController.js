c7f49ef30e9958ac1e9c1a6337178d2efa9b406f
quoc loc <loc842211@gmail.com>
2023-09-08 22:17:48 +0700

    viet API lay all nguoi dung

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,6 +23,24 @@ let handleLogin = async (req,res)=>{
         user:userData.user ? userData.user: {}
      })
 }
+let handleGetAllUsers =async(req,res)=>{
+    let id =req.body.id;//all,single
+    if(!id){
+        return res.status(200).json({
+            errCode:1,
+            errMessage:'Missing required pqrameters',
+            users:[]
+        })
+    }
+
+    let users =await userService.getAllUsers(id) 
+    return res.status(200).json({
+        errCode:0,
+        errMessage:'ok',
+        users
+    })
+}
 module.exports={
-    handleLogin:handleLogin
+    handleLogin:handleLogin,
+    handleGetAllUsers:handleGetAllUsers
 }
--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-import connectDB from './config/connectDB'
+import connectDB from './config/connectDB';
+import cors from 'cors'
+
 require("dotenv").config();
 
 let app = express();
+app.use(cors({origin:true}))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
