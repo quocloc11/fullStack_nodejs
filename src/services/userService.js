b78ecd4d019dee6e54d14cbd5a78b9ebf2743a13
quoc loc <loc842211@gmail.com>
2023-09-08 11:42:27 +0700

    API

---
+++ src/services/userService.js
@@ -0,0 +1,67 @@
+import bcrypt from 'bcryptjs';
+import db from "../models/index"
+
+let handleUserLogin = (email,password)=>{
+      return new Promise(async(resolve,reject)=>{
+        try{
+            let userData={}
+            let isExist=await checkUserEmail(email)
+            if(isExist){
+                let user =await db.User.findOne({
+                    attributes: ['email','roleId','password'],
+                    where:{email:email},
+                    raw:true
+                })
+                if(user){
+                   let check= await bcrypt.compareSync(password,user.password);
+                  
+                   if(check){
+                    userData.errCode = 0;
+                    userData.errMessage = 'ok',
+                    
+                    delete user.password;
+                    userData.user=user;
+                   }else{
+                    userData.errCode =3;
+                    userData.errMessage ="Wrong password"
+                   }
+                  
+                   
+                }
+                else{
+                    userData.errCode = 2;
+                    userData.errMessage = `User not found`
+                }
+
+            }else{
+                userData.errCode = 1;
+                userData.errMessage = `You's Email isn't exit`
+                
+            }
+            resolve(userData)
+        }catch(e){
+            reject(e)
+        }
+      }) 
+}
+
+let checkUserEmail = (userEmail) =>{
+    return new Promise(async(resolve,reject)=>{
+        try{
+          let user = await db.User.findOne({
+            where:{email:userEmail}
+          })
+          if(user){
+            resolve(true)
+          }else{
+            resolve(false)
+          }
+        }
+        catch(e){
+               reject(e)
+        }
+       })
+}
+module.exports ={
+    handleUserLogin:handleUserLogin
+}
