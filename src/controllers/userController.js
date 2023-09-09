21bdbfd7feee65afe3dcf2299a41fa242af55a23
quoc loc <loc842211@gmail.com>
2023-09-09 17:39:59 +0700

    api delete edit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,7 +24,7 @@ let handleLogin = async (req,res)=>{
      })
 }
 let handleGetAllUsers =async(req,res)=>{
-    let id =req.body.id;//all,single
+    let id =req.query.id;//all,single
     if(!id){
         return res.status(200).json({
             errCode:1,
@@ -40,7 +40,32 @@ let handleGetAllUsers =async(req,res)=>{
         users
     })
 }
+let handleCreateNewUser =async (req,res)=>{
+   let message = await userService.createNewUser(req.body);
+   return res.status(200).json(message)
+}
+let handleDeleteUser =async (req,res)=>{
+    if(!req.body.id){
+        return res.status(200).json({
+            errCode:1,
+            errMessage:"Missing required"
+        })
+    }
+    let message = await userService.deleteUser(req.body.id);
+    return res.status(200).json(message)
+}
+
+let handleEditUser=async (req,res)=>{
+    let data=req.body
+    let message= await userService.updateUserData(data)
+    return res.status(200).json(message)
+    
+
+}
 module.exports={
     handleLogin:handleLogin,
-    handleGetAllUsers:handleGetAllUsers
+    handleGetAllUsers:handleGetAllUsers,
+    handleCreateNewUser:handleCreateNewUser,
+    handleEditUser:handleEditUser,
+    handleDeleteUser:handleDeleteUser
 }
