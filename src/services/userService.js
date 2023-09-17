c4b0ee1815830cbf639f6cac580c1f85cd44f2c8
quoc loc <loc842211@gmail.com>
2023-09-17 17:03:07 +0700

    debugs create redux

--- src/services/userService.js
+++ src/services/userService.js
@@ -122,9 +122,10 @@ let createNewUser =(data) =>{
           firstName:data.firstName,
           lastName: data.lastName,
           address:data.address,
-          phonenumber:data.phoneumber,
-          gender:data.gender === '1' ? true : false,
+          phonenumber:data.phonenumber,
+          gender:data.gender ,
           roleId:data.roleId,
+          positionId:data.positionId
       })
       resolve({
         errCode:0,
