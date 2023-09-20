b944fd22b55524bb0d7d3a3ddff080ce2ed6a423
quoc loc <loc842211@gmail.com>
2023-09-20 22:34:50 +0700

    load thong tin bac si noi bat qua tuan co anh

--- src/route/web.js
+++ src/route/web.js
@@ -1,6 +1,8 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 import userController from "../controllers/userController"
+import doctorController from "../controllers/doctorController"
+
 let router =express.Router();
 
 // let initWebRoutes = (app) =>{
@@ -24,6 +26,7 @@ let initWebRoutes = (app)=>{
     router.put('/api/edit-user',userController.handleEditUser)
     router.delete('/api/delete-user',userController.handleDeleteUser)
     router.get('/api/allcode',userController.getAllCode)
+    router.get('/api/top-doctor-home',doctorController.getTopDoctorHome)
 
 
     return app.use("/",router)
