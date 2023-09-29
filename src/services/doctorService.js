b6276616eb2767d2dcc680fe83036bad972da109
quoc loc <loc842211@gmail.com>
2023-09-29 15:36:11 +0700

    xem chi tiet bac si

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -86,7 +86,7 @@ let getDetailDoctorById = (iputId)=>{
                         id:iputId
                     },
                     attributes:{
-                        exclude:['password','image']
+                        exclude:['password']
                     },
                     include:[
                         {
@@ -96,9 +96,14 @@ let getDetailDoctorById = (iputId)=>{
                         {model:db.Allcode,as: 'positionData',attributes:['valueEn','valueVi']},
 
                     ],
-                    raw:true,
+                    raw:false,
                     nest:true
                 })
+                if(data && data.image){
+                     data.image = new Buffer(data.image,'base64').toString('binary')
+
+                }
+                if(!data) data={}
                 resolve({
                     errCode:0,
                     data:data
