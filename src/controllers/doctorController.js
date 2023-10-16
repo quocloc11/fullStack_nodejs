6ea4eb08ac0c808a20c70a48778066c2103fb7b9
quoc loc <loc842211@gmail.com>
2023-10-16 14:17:49 +0700

    viet API load dong thong tin bac si

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -99,6 +99,22 @@ let getExtraInforDoctorById =async (req,res)=>{
         })
     }
 }
+
+let getProfileDoctorById =async (req,res)=>{
+    try{
+        
+        let infor =await doctorService.getProfileDoctorById(req.query.doctorId);
+        return res.status(200).json(
+            infor
+        )
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode:-1,
+            errMessage:'Error from the server'
+        })
+    }
+}
 module.exports={
     getTopDoctorHome:getTopDoctorHome,
     getAllDoctors:getAllDoctors,
@@ -106,5 +122,6 @@ module.exports={
     getDetailDoctorById:getDetailDoctorById,
     bulkCreateSchedule:bulkCreateSchedule,
     getScheduleByDate:getScheduleByDate,
-    getExtraInforDoctorById:getExtraInforDoctorById
+    getExtraInforDoctorById:getExtraInforDoctorById,
+    getProfileDoctorById:getProfileDoctorById
 }
