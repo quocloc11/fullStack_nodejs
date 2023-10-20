1bc47821d10cc1ec3d72f97965e34c0b05cfc181
quoc loc <loc842211@gmail.com>
2023-10-21 00:50:48 +0700

    tao base giao dien quan ly danh sach lich hen kham benh

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -115,6 +115,22 @@ let getProfileDoctorById =async (req,res)=>{
         })
     }
 }
+
+let getListPatientForDoctor =async(req,res)=>{
+    try{
+        
+        let infor =await doctorService.getListPatientForDoctor(req.query.doctorId,req.query.date);
+        return res.status(200).json(
+            infor
+        )
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode:-1,
+            errMessage:'Error from the server'
+        })
+    }
+}
 module.exports={
     getTopDoctorHome:getTopDoctorHome,
     getAllDoctors:getAllDoctors,
@@ -123,5 +139,6 @@ module.exports={
     bulkCreateSchedule:bulkCreateSchedule,
     getScheduleByDate:getScheduleByDate,
     getExtraInforDoctorById:getExtraInforDoctorById,
-    getProfileDoctorById:getProfileDoctorById
+    getProfileDoctorById:getProfileDoctorById,
+    getListPatientForDoctor:getListPatientForDoctor
 }
--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -13,7 +13,8 @@ let postBookAppointment = (data) =>{
     return new Promise(async (resolve,reject)=>{
         try{
             if(!data.email || !data.doctorId || !data.timeType || !data.timeType || !data.date
-                || !data.fullName
+                || !data.fullName || !data.selectedGender
+                || !data.address
                 ){
                 resolve({
                     errCode:1,
@@ -34,7 +35,10 @@ let postBookAppointment = (data) =>{
                     where:{email:data.email},
                     defaults:{
                         email:data.email,
-                        roleId:'R3'
+                        roleId:'R3',
+                        gender: data.selectedGender,
+                        address :data.address,
+                        firstName:data.fullName
                     }
                 })
 
